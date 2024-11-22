0e355ce4109715329b72c41124d6f08af61c3a97
Ladzsz <londonmason602@gail.com>
2024-11-22 12:57:43 -0500

    finished nesecary functions

--- linkedList.js
+++ linkedList.js
@@ -58,6 +58,140 @@ export class linkedlist {
     tails() {
         console.log(this.tail);
     }
+
+    //return node at given index function
+    at(index) {
+        //if statement incase index isnt real
+        if (index < 0 || index >= this.size) {
+            console.log("Index does not exist.")
+        } 
+
+        //initalziing the current node and index
+        let currentNode = this.head;
+        let currentIndex = 0;
+
+        //while loop to find the idex
+        while (currentNode < index) {
+            currentNode = currentNode.nextNode;
+            currentIndex++;
+        }
+
+        //returning the current node value
+        return currentNode.value;
+    }
+
+    //remove last element from the list
+    pop() {
+        // If the list is empty, nothing to remove
+        if (!this.head) {
+            console.log("List is empty");
+            return null;
+        }
+
+        // If the list has only one node
+        if (this.head === this.tail) {
+            const removedValue = this.head.value;
+            this.head = null;
+            this.tail = null;
+            this.size--;
+            return removedValue;
+        }
+
+        // Traverse to find the second-to-last node
+        let current = this.head;
+        while (current.nextNode !== this.tail) {
+            current = current.nextNode;
+        }
+
+        // Store the value to return
+        const removedValue = this.tail.value; 
+
+        // Update the tail to the second-to-last node
+        this.tail = current;       
+
+        // Setting next node back to null
+        this.tail.nextNode = null;
+
+        // Decrease the size
+        this.size--;               
+
+        //return the removed value
+        return removedValue;       
+    }
+
+    //function checking if value is in the list
+    contains(value) {
+        //inizaliing current node
+        let current = this.head;
+
+        //looping through list
+        while (current) {
+            // Check if the current node's value matches
+            if (current.value === value) {  
+                return true;     
+            }
+
+            //moving to next node
+            current = current.nextNode;
+        }
+
+        //return false if not found
+        return false;
+    }
+
+    //function to return trhe index of the node containing value and return null if not found
+    find(value) {
+        //initliazing head and index
+        let current = this.head;  
+        let currentIndex = 0;     
+
+        //while loop to search through list
+        while (current) {         
+            //if value matches return index
+            if (current.value === value) {  
+                return currentIndex;        
+            }
+            //moving to next node and incrementing the index
+            current = current.nextNode;     
+            currentIndex++;                 
+        }
+
+        //returning null if not found
+        return null;
+    }
+
+    //function to represent linked list objects as strings
+    toString() {
+        //initliazing head and result
+        let current = this.head;  
+        let result = "";  
+
+        //if list empty return message
+        if (!current) {
+            return "List is empty";  
+        }
+
+        //looping through the list
+        while (current) {
+
+            // Add the current node's value
+            result += current.value;  
+
+            // Add a separator if there's a next node
+            if (current.nextNode) {   
+                result += " -> ";
+            }
+
+            // Move to the next node
+            current = current.nextNode;
+        }
+
+        // Adding null to end
+        result += " -> null ";
+
+        //return the result
+        return result;
+    }
 }
 
 //class for node
